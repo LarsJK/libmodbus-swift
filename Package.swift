eca5b698700fd8c4bc6402eb88a9f538da5b56fd
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2019-07-24 14:38:24 +0200

    Update to latest swift syntax

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
     name: "LibModbus",
     dependencies: [
-        .Package(url: "https://github.com/LarsJK/Clibmodbus.git", majorVersion: 2),
+        .package(url: "https://github.com/LarsJK/Clibmodbus.git", from: "2.0.0"),
     ]
 )
